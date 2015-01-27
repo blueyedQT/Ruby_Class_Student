3d476fcb20e8b6a364e984e879d66c3264b13e4f
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-27 14:15:44 -0800

    Saving current progress

---
+++ class_student.rb
@@ -0,0 +1,10 @@
+class Student
+	def initialize(name, location, belt)
+		@name = name
+		@dojo_location = location
+		@belt_level = belt
+	end
+end
+
+# Katrina = Student.new("Katrina", "Bellevue", "Black")
+# puts Katrina
