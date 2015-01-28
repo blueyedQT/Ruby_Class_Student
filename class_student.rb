0ba85b0280959b6393e148ad10a8eefe0e32c6d1
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-28 13:43:26 -0800

    Completed!

--- class_student.rb
+++ class_student.rb
@@ -1,10 +1,31 @@
 class Student
-	def initialize(name, location, belt)
+	attr_accessor :dojo_location, :belt_level
+	def initialize (name, location, belt)
 		@name = name
 		@dojo_location = location
 		@belt_level = belt
 	end
+	def displayAll
+		puts "Student name: %s" % @name
+		puts "Dojo Location: %s" % @dojo_location
+		puts "Belt Level: %s" % @belt_level
+	end
+	# def dojo_location
+	# 	return @dojo_location
+	# end
+	# def dojo_location=(location)
+	# 	@dojo_location = location
+	# end
+	# def belt_level
+	# 	return @name
+	# end
+	# def belt_level=(belt)
+	# 	@belt_level = belt
+	# end
 end
 
-# Katrina = Student.new("Katrina", "Bellevue", "Black")
-# puts Katrina
+katrina = Student.new("Katrina", "Bellevue", "Black")
+katrina.displayAll
+katrina.dojo_location = "Seattle"
+katrina.belt_level = "Double Black"
+katrina.displayAll
